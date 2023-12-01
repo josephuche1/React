2b1f844adb37cff99284ca9880dacd29183e9cb3
PARSIVALJ <uchejesusboy@gmail.com>
2023-12-01 23:42:13 +0200

    Add styles to react app using materials ui

--- keeper-app-part-1-starting/src/components/CreateArea.jsx
+++ keeper-app-part-1-starting/src/components/CreateArea.jsx
@@ -1,12 +1,20 @@
-import React from "react";
+import React, {useState} from "react";
+import AddIcon from '@mui/icons-material/Add';
+import Fab from '@mui/material/Fab';
+import Zoom from '@mui/material/Zoom';
 
 function CreateArea(props) {
+  const [newState, setState] = useState(false);
+
+  function handleClick(){
+    setState(true);
+  }
   return (
     <div>
       <form onSubmit={props.handleSubmit} className="create-note">
-        <input name="title" placeholder="Title" onChange={props.handleChange} value={props.note.title}/>
-        <textarea name="content" placeholder="Take a note..." rows="3" onChange={props.handleChange} value={props.note.content} />
-        <button type="submit">Add</button>
+        {newState && <input name="title" placeholder="Title" onChange={props.handleChange} value={props.note.title}/>}
+        <textarea name="content" placeholder="Take a note..." rows={newState? "3" : "1" } onChange={props.handleChange} value={props.note.content} onClick={handleClick}/>
+        <Zoom in={newState}><Fab type={"submit"}><AddIcon /></Fab></Zoom>
       </form>
     </div>
   );
