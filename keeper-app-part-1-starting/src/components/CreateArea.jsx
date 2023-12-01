453fc1c99a84fdf81d236f6090fa86a81ea2d02c
PARSIVALJ <uchejesusboy@gmail.com>
2023-12-01 22:37:37 +0200

    Edit styles and createArea component

--- keeper-app-part-1-starting/src/components/CreateArea.jsx
+++ keeper-app-part-1-starting/src/components/CreateArea.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function CreateArea(props) {
   return (
     <div>
-      <form onSubmit={props.handleSubmit}>
+      <form onSubmit={props.handleSubmit} className="create-note">
         <input name="title" placeholder="Title" onChange={props.handleChange} value={props.note.title}/>
         <textarea name="content" placeholder="Take a note..." rows="3" onChange={props.handleChange} value={props.note.content} />
         <button type="submit">Add</button>
