c071fc0f5d7ce1fb2be72b5b2dcc09958235b971
PARSIVALJ <uchejesusboy@gmail.com>
2023-12-01 21:26:31 +0200

    Add feature to allow users to add and delete notes from the list

--- keeper-app-part-1-starting/src/components/App.jsx
+++ keeper-app-part-1-starting/src/components/App.jsx
@@ -1,19 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Note from "./Note";
 import Footer from "./Footer";
-import notes from "../notes";
+// import noti from "../notes";
+import CreateArea from "./CreateArea";
 
 function App(){
+    const [note, SetNote] = useState({
+        title:"",
+        content:""
+    });
+    const [notes, updateNotes] = useState([]);
+
+    function handleChange(event){
+        const {name, value} = event.target;
+        
+        SetNote((note) => {
+            return {
+                ...note,
+                [name]: value
+            };
+        });
+    }
+
+    function handleSubmit(event){
+        updateNotes((notes) => {
+            return [...notes, note];
+        });
+
+        SetNote(() => {
+            return {
+                title:"",
+                content:""
+            }
+        })
+        event.preventDefault();
+    }
+
+    function deleteNote(id){
+        updateNotes(() => {
+            return notes.filter((note, index) => {
+                return index !== id;
+            });
+        })
+    }
     return (
         <div>
             <Header />
-            {notes.map((note) => {
+            <CreateArea 
+                handleChange={handleChange}
+                handleSubmit={handleSubmit}
+                note={note}
+            />
+            {notes.map((note, index) => {
                 return (
                     <Note 
-                        key={note.key}
+                        key={index}
+                        id={index}
                         title={note.title}
                         content={note.content}
+                        delete={deleteNote}
                     />
                 )
             })}
---
+++ keeper-app-part-1-starting/src/components/CreateArea.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function CreateArea(props) {
+  return (
+    <div>
+      <form onSubmit={props.handleSubmit}>
+        <input name="title" placeholder="Title" onChange={props.handleChange} value={props.note.title}/>
+        <textarea name="content" placeholder="Take a note..." rows="3" onChange={props.handleChange} value={props.note.content} />
+        <button type="submit">Add</button>
+      </form>
+    </div>
+  );
+}
+
+export default CreateArea;
