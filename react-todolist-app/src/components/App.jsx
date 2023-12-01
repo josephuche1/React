daa0fb25b35a61f3f56184e7fe4bc9e569128b79
PARSIVALJ <uchejesusboy@gmail.com>
2023-12-01 17:03:47 +0200

    Add components for the list item, debug code and render app

---
+++ react-todolist-app/src/components/App.jsx
@@ -0,0 +1,58 @@
+import React, {useState} from "react";
+import ListItem from "./ListItem";
+
+
+function createItems(item){
+    return (
+        <ListItem 
+            key={item.id}
+            item={item.item}
+        />
+    );
+}
+
+function App() {
+
+  const [item, setItem] = useState("");
+  const [items, setItems] = useState([]);
+
+  function handleChange(event){
+      const {value} = event.target;
+      setItem(value);
+  }
+
+  function handleSubmit(event){
+      const newItem = {
+         id: items.length,
+         item: item
+      }
+      setItems((items) => {
+        return [...items, newItem];
+      });
+      setItem("");
+      event.preventDefault();
+  }
+
+
+
+  return (
+    <div className="container">
+      <div className="heading">
+        <h1>To-Do List</h1>
+      </div>
+      <form className="form" onSubmit={handleSubmit}>
+        <input onChange={handleChange} type="text" value={item}/>
+        <button type="submit">
+          <span>Add</span>
+        </button>
+      </form>
+      <div>
+        <ul>
+          {items.map(createItems)}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
